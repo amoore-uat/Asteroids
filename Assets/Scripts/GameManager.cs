6360c55ceef8eee3914294bd83a1fee9d3c21a6b
Adam <amoore@uat.edu>
2020-01-27 12:14:15 -0700

    Screaming into the eternal void

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score = 0;
+    public bool isPaused = false;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("I tried to create a second game manager.");
+        }
+    }
+
+}
